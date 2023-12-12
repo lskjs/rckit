382b90446732dc7d84acedff8364792b8dfa6c3f
Igor Suvorov <hi@isuvorov.com>
2023-12-12 08:03:24 +0300

    fix(link): pass all compoent to context

--- libs/link/src/ComponentProvider.tsx
+++ libs/link/src/ComponentProvider.tsx
@@ -1,18 +1,21 @@
 import React, { createContext } from 'react';
 
+const Router = null;
 const Link = 'a';
 const Image = 'img';
 
 export const ComponentContext = createContext({
+  Router,
   Link,
   Image,
 });
 type ComponentProviderProps = React.PropsWithChildren<{
+  Router: any;
   Link: any;
   Image: any;
 }>;
 
 // eslint-disable-next-line no-shadow
-export const ComponentProvider = ({ Link, Image, children }: ComponentProviderProps) => (
-  <ComponentContext.Provider value={{ Link, Image }}>{children}</ComponentContext.Provider>
+export const ComponentProvider = ({ children, ...value }: ComponentProviderProps) => (
+  <ComponentContext.Provider value={value}>{children}</ComponentContext.Provider>
 );
