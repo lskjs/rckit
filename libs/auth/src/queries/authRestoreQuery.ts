5d4997eb9d1c8b0b2fed0673f47f740b55044de3
Igor Suvorov <hi@isuvorov.com>
2023-12-12 13:27:49 +0300

    fix(auth): add authRestoreQuery

---
+++ libs/auth/src/queries/authRestoreQuery.ts
@@ -0,0 +1,29 @@
+import { Err } from '@lsk4/err';
+import type { ApiClientOptions } from '@rckit/api-client';
+import { apiClient as defaultApiClient } from '@rckit/api-client';
+
+import { Otp, Session } from '../types.js';
+
+export interface AuthRestoreParams {
+  email: string;
+  password: string;
+}
+export interface AuthRestoreResponse {
+  data: {
+    otp: Otp;
+    session: Session;
+  };
+}
+export async function fetchAuthRestore(
+  params: AuthRestoreParams,
+  { apiClient = defaultApiClient }: ApiClientOptions = {},
+) {
+  // TODO: change any
+  const { data } = await apiClient.request<any, AuthRestoreResponse>({
+    method: 'post',
+    url: '/api/auth/restore',
+    data: params,
+  });
+  if (!data?.otp && !data?.session) throw new Err('incorrectBackendResponse', { data });
+  return data;
+}
--- libs/auth/src/queries/index.tsx
+++ libs/auth/src/queries/index.tsx
@@ -3,3 +3,4 @@ export * from './authOtpQuery.js';
 export * from './authSessionQuery.js';
 export * from './authSignupQuery.js';
 export * from './authWebappQuery.js';
+export * from './authRestoreQuery.js';
