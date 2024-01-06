0ddc2e6056c2ff064f67c0b8c783898c3d2cf38f
Igor Suvorov <hi@isuvorov.com>
2024-01-06 13:14:27 +0300

    fix(breadcrumbs): findBreadcrumbs deep

--- libs/breadcrumbs/src/utils.tsx
+++ libs/breadcrumbs/src/utils.tsx
@@ -2,12 +2,14 @@ import { MenuItem } from './types.js';
 
 export const isActive = (item: MenuItem, activeHref?: string) => item?.href === activeHref;
 
-export const findBreadcrumbs = (items: MenuItem[], activeHref: string) => {
+export const findBreadcrumbs = (items: MenuItem[], activeHref: string): MenuItem[] => {
   // eslint-disable-next-line no-restricted-syntax
   for (const item of items) {
     if (item.items) {
-      const subitem = item.items.find((it) => isActive(it, activeHref));
-      if (subitem) return [item, subitem];
+      const subbreads = findBreadcrumbs(item.items, activeHref);
+      if (subbreads.length) return [item, ...subbreads];
+      // const subitem = item.items.find((it) => isActive(it, activeHref));
+      // if (subitem) return [item, subitem];
     }
     if (isActive(item, activeHref)) return [item];
   }
