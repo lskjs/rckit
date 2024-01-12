51f19f0bbc956b21f47e39d1edf7f3125d918b69
Igor Suvorov <hi@isuvorov.com>
2024-01-12 21:23:27 +0300

    feat(auth): improve types useAppUser

--- libs/auth/src/AppSession/useAppUser.tsx
+++ libs/auth/src/AppSession/useAppUser.tsx
@@ -4,14 +4,19 @@ export const useAppUser = () => {
   const { session } = useAppSession();
   const user = session?.user;
   if (!user) return null;
+
+  // @ts-ignore
+  const _id = user.id;
+  const isAdmin = user.role === 'admin';
+  // @ts-ignore
+  const title = user.name || user.email;
+  // @ts-ignore
+  const { avatar } = user;
   return {
     ...user,
-    // @ts-ignore
-    _id: user.id,
-    isAdmin: user?.role === 'admin',
-    // @ts-ignore
-    title: user.name || user.email,
-    // @ts-ignore
-    avatar: user.avatar,
+    _id,
+    isAdmin,
+    title,
+    avatar,
   };
 };
